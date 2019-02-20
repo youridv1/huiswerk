d80d6795a6ce7194e87ab45b730002580ee6c66a
Youri de Vor <youri.devor@student.hu.nl>
2019-02-20 12:46:48 +0100

    3_9 af

--- 3_9.cpp
+++ 3_9.cpp
@@ -13,7 +13,6 @@ void improveLoveMessage(char & message){
     srand(time(NULL));
     for (int i = 0; i < 3 ; ++i) {
         int index = rand() % 7;
-        cout << index << endl;
         indexes.push_back(index);
     }
     for(size_t j = 0; j < indexes.size(); j++) {
